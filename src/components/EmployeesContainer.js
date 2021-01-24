2ba7b0b700deb779b6db3d6886532cbb56801482
aubrey-heim <aubrey.l.heim@gmail.com>
2021-01-24 15:23:34 -0800

    made search functional

--- src/components/EmployeesContainer.js
+++ src/components/EmployeesContainer.js
@@ -7,6 +7,7 @@ import ListHeader from "./ListHeader"
 class EmployeesContainer extends Component {
   state = {
     employees: [],
+    employeesShown: [],
     search:"",
     sortBy: "",
     sortDirection: ""
@@ -18,21 +19,30 @@ class EmployeesContainer extends Component {
 
   getEmployees = query => {
     API.search(query)
-      .then(res => this.setState({ results: res.data.results }))
+      .then(res => this.setState({ employees: res.data.results, employeesShown: res.data.results}))
       .catch(err => console.log(err));
   };
 
   handleInputChange = event => {
-    const name = event.target.name;
-    const value = event.target.value;
+    const search = event.target.value;
     this.setState({
-      [name]: value
-    });
+      search: search
+    })
   };
 
   handleFormSubmit = event => {
     event.preventDefault();
-
+    let phoneSearch = this.state.search.replace(/[^0-9]/g, '')
+    let lowerSearch = this.state.search.toLowerCase()
+    let filteredEmployees = this.state.employees.filter(employee => employee.name.first.toLowerCase().includes(lowerSearch) 
+    || employee.name.last.toLowerCase().includes(lowerSearch)
+    || employee.email.includes(lowerSearch)
+    || employee.cell.replace(/[^0-9]/g, '').includes(phoneSearch)
+    )
+    console.log(filteredEmployees)
+    this.setState({
+      employeesShown: filteredEmployees
+    })
   };
 
   handleSort = event =>{
@@ -48,7 +58,7 @@ class EmployeesContainer extends Component {
           handleInputChange={this.handleInputChange}
         />
         <ListHeader handleSort={this.handleSort}/>
-        {this.state.employees.map(item => <EmployeeList {...item} key={item.login.uuid}/>)}
+        {this.state.employeesShown.map(item => <EmployeeList {...item} key={item.login.uuid}/>)}
       </div>
     );
   }
