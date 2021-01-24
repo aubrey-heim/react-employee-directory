52cbb90f5f88e30c79cd429e5f1550d001911411
aubrey-heim <aubrey.l.heim@gmail.com>
2021-01-24 14:49:55 -0800

    updated class to className

--- src/components/ListHeader.js
+++ src/components/ListHeader.js
@@ -5,9 +5,9 @@ function ListHeader() {
     <div>
       <ul className="list-group list-group-horizontal row text-center" style={{fontSize:"20px"}}>
         <li className="list-group-item list-group-item-secondary col-2">Photo</li>
-        <li className="list-group-item list-group-item-secondary col-3">Name <i class="fas fa-sort"></i></li>
-        <li className="list-group-item list-group-item-secondary col-3">Phone <i class="fas fa-sort"></i></li>
-        <li className="list-group-item list-group-item-secondary col-4">Email <i class="fas fa-sort"></i></li>
+        <li className="list-group-item list-group-item-secondary col-3">Name <i className="fas fa-sort"></i></li>
+        <li className="list-group-item list-group-item-secondary col-3">Phone <i className="fas fa-sort"></i></li>
+        <li className="list-group-item list-group-item-secondary col-4">Email <i className="fas fa-sort"></i></li>
       </ul>
     </div>
   );
